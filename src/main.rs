7b182a18554d416d1da27585659c0b69128af4f6
DiegoDuaS <diego.slowing@gmail.com>
2024-07-11 20:43:28 -0600

    feat: polygon 1

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,25 @@ fn main() -> io::Result<()> {
     // Dibujar algo en el framebuffer
     framebuffer.clear();
 
+    let points1 = vec![
+        Vector3::new(165.0, 380.0, 0.0),
+        Vector3::new(185.0, 360.0, 0.0),
+        Vector3::new(180.0, 330.0, 0.0),
+        Vector3::new(207.0, 345.0, 0.0),
+        Vector3::new(233.0, 330.0, 0.0),
+        Vector3::new(230.0, 360.0, 0.0),
+        Vector3::new(250.0, 380.0, 0.0),
+        Vector3::new(220.0, 385.0, 0.0),
+        Vector3::new(205.0, 410.0, 0.0),
+        Vector3::new(193.0, 383.0, 0.0),
+    ];
+
+    framebuffer.set_current_color(0xFFFFFF00);
+    framebuffer.fill_polygon(&points1, 0xFFFFFF00);
+
+    framebuffer.set_current_color(0xFFFFFFFF);  
+    framebuffer.draw_polygon(&points1);
+
     // Guardar como archivo BMP
     let filename = "poligonos.bmp";
     write_bmp_file(filename, width as u32, height as u32, &framebuffer.buffer)?;
