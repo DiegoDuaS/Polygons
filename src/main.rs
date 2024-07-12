8bba8d36ee4739ea6224eb3fc9df2091c9945d12
DiegoDuaS <diego.slowing@gmail.com>
2024-07-11 20:41:24 -0600

    feat: added files to draw the polygons

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod framebuffer;
+mod bmp;
+mod line;
+mod polygon;
+
+use std::io;
+use framebuffer::Framebuffer;
+use crate::bmp::write_bmp_file; 
+use nalgebra::Vector3;
+use crate::line::Line;
+use crate::polygon::Polygon;
+
+fn main() -> io::Result<()> {
+    let width = 500;
+    let height = 500;
+    let mut framebuffer = Framebuffer::new(width, height);
+
+    // Dibujar algo en el framebuffer
+    framebuffer.clear();
+
+    // Guardar como archivo BMP
+    let filename = "poligonos.bmp";
+    write_bmp_file(filename, width as u32, height as u32, &framebuffer.buffer)?;
+
+    println!("Archivo BMP generado: {}", filename);
+    Ok(())
+}
